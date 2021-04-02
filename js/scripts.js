80c125cecd9e0b569cab3aefe79d688289cdff58
Rami <ramiahmedportland@gmail.com>
2021-04-02 15:33:35 -0700

    Add ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,16 @@
 function userArray(number){
   let userArray = [];
    for (let item = 0; item <= number; item ++) {
-    }
-   let itemToWords = item.toString();
-    if(itemToWords.includes(3)) {
-      userArray.push("Won't you be my neighbor?");
-    } else if (itemToWords.includes(2)) {
-      userArray.push("Boop!")
-    } else if (itemToWords.includes(1)){
-      userArray.push("Beep!")
-    } else {
-      userArray.push(itemToWords);
+     let itemToWords = item.toString();
+      if(itemToWords.includes(3)) {
+        userArray.push("Won't you be my neighbor?");
+      } else if (itemToWords.includes(2)) {
+        userArray.push("Boop!")
+      } else if (itemToWords.includes(1)){
+        userArray.push("Beep!")
+      } else {
+        userArray.push(itemToWords);
+      }
     }
 
     return userArray;
@@ -27,6 +27,7 @@ $(document).ready(function(){
   $("form#form").submit(function(event) {
     event.preventDefault();
     const homeAdress = $("input#luckyNumber").val();
+    $(".outcome").text(userArray(homeAdress));
     $("#result").show();
   });
 });
