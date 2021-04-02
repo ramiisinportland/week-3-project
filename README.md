bd2860e1787ed2652068ef523446ecccb20986d1
Rami <ramiahmedportland@gmail.com>
2021-04-02 12:25:59 -0700

    Add another test, update js

--- README.md
+++ README.md
@@ -9,9 +9,13 @@ Test: "It should return Won't you be my neighbor? if user input 3"
 Code: userArray(3)
 Expected Output: "Won't you be my neighbor?"
 
-Test: "It should returnBoop!" 
+Test: "It should return Boop!" 
 Code: userArray(2)
 Expected Output: "Boop!"
+
+Test: "It should return Beep!" 
+Code: userArray(1)
+Expected Output: "Beep!"
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,19 @@
 
 function userArray(number){
   let userArray = [];
-  for (let item = 0; item <= number; item ++) {
+   for (let item = 0; item <= number; item ++) {
+    }
+   let itemToWords = item.toString();
+    if(itemToWords.includes(3)) {
+      userArray.push("Won't you be my neighbor?");
+    } else if (itemToWords.includes(2)) {
+      userArray.push("Boop!")
+    } else if (itemToWords.includes(1)){
+      userArray.push("Beep!")
+    } else {
+      userArray.push(itemToWords);
+    }
+
+    return userArray;
   }
-   let itemToWords = item.string();
-    if(userArray.includes(3)) {
-    userArray.push("Won't you be my neighbor?")
- } else if (itemToWords.includes(2)) {
-   userArray.push("Boop!")
- }
-   return userArray;
- }
+ 
